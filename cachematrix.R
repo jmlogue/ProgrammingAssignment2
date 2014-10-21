411a5ba6c4a9617eedb58eccdb6000d1f8ac21a8
jmlogue <jmlogue@lbl.gov>
2014-10-20 21:41:28 -0700

    Second update on homework

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,8 @@
 ##The first fuctions creates a special matrix object that can cache its inverse. The second function
 ##computes
 
-## Write a short comment describing this function
-##Short comment descriping the function: this is a funtion that puts a matrix in chache memmory
+
+##This is a funtion that puts the inverse of a matrix in chache memmory. 
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -22,7 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## This function looks to see if the inverse of the matrix already exists by checking to see if the varible is a
-## null.
+## null. If it is not, it finds the inverse of the matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getinverse()
